4f8c2b6b4d565d38520a6d9b9eb7acc98f989f2c
김근호 <kimkgh8@gmail.com>
2023-04-11 08:29:41 +0900

    ✨ Feat : Create Archving Kim's Project

---
+++ backend/src/test/java/kim/archive/archive/ArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package kim.archive.archive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
