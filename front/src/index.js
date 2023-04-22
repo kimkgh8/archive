5bc927bae104508b04d7d963d1a5bc541809b000
김근호 <kimkgh8@gmail.com>
2023-04-22 16:39:02 +0900

    ✨ Feat : 기본 구조 변경

--- front/src/index.js
+++ front/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './js/App';
+import Login from './js/components/Login'
+
+const login = ReactDOM.createRoot(document.getElementById('login'));
+login.render(
+	<React.StrictMode>
+	<Login />
+	</React.StrictMode>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +18,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
